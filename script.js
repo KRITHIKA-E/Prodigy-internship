d48207ec3b5398994c2d38bb8ea621d6da83675e
KRITHIKA-E <145641320+KRITHIKA-E@users.noreply.github.com>
2024-07-09 12:23:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+console.log('JavaScript file loaded');
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
